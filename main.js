41d03e705c4e060a435d3dd75c7ed0838327ec94
Sean Coh <scoh21@basecampcodingacademy.org>
2021-04-19 16:22:05 -0500

    countdown function

--- main.js
+++ main.js
@@ -1,14 +1,39 @@
 let easyList = ['dog', 'cat', 'homie', 'primo']
 let words = document.querySelector('.words')
-let start = document.querySelector('#startButton')
+let startButton = document.querySelector('#startButton')
+let tempo = document.querySelector('.time');
+let timerDiv =document.querySelector(".time");
+let seconds = 60;
 
 console.log(words)
 
 
-start.addEventListener('click', () => {
-    alert("Hello World!");
-    console.log('fml')
-    let word = Math.random(easyList)
-    console.log(word)
-    words.textContent(word)
-})
+// startButton.addEventListener('click', () => {
+//     alert("Hello World!");
+//     console.log('fml')
+//     let word = Math.random(easyList)
+//     console.log(word)
+//     words.textContent(word)
+// })
+
+startButton.addEventListener("click", () => {
+    countDown();
+    button.disabled = true;	
+});
+
+function countDown() {
+    let timer = setInterval(function(){
+        startButton.disabled = true;
+        seconds--;
+        tempo.innerHTML = seconds;
+        if (seconds === 0) {
+            button.disabled = false;
+            clearInterval(timer);
+            seconds = 60;
+            timerDiv.innerHTML = "60";
+            button.disabled = false;	
+        }
+    }, 1000);
+}
+
+
