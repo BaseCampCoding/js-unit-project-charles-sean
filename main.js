39720ab7ce70b2914f428f46c98d0d7fda5fdfc6
Charles-Mardis <72397256+Charles-Mardis@users.noreply.github.com>
2021-04-19 15:46:04 -0500

    working on checking if user gets right or wrong

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 let easyList = ['dog', 'cat', 'homie', 'primo']
 let words = document.querySelector('.words')
 let start = document.querySelector('#startButton')
+let answer = document.querySelector('#user__input')
 
-console.log(words)
 
 
 start.addEventListener('click', () => {
-    alert("Hello World!");
-    console.log('fml')
-    let word = Math.random(easyList)
+    let word = Math.floor(Math.random() * easyList.length)
     console.log(word)
-    words.textContent(word)
+    words.textContent = word
 })
+
+answer.addEventListener('spacebar', () => {
+    if (answer == words){
+        console.log('fml')
+    }else{
+        console.log('this one i bet')
+    }
+})
+
