4fdce94d6fe8cd9334783eaa198509fbe4cf61df
Sean Coh <scoh21@basecampcodingacademy.org>
2021-04-20 14:06:08 -0500

    fixing answer button

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-let easyList = ['dog', 'cat', 'homie', 'primo']
+let easyList = ['dog', 'cat', 'homie', 'primo', "ice", "idea", "if", "important",
+    "in", "increase", "inside", "into", "introduce", "invent", "iron", "invite", 
+    "is", "island", "it", "its"]
 let words = document.querySelector('.words')
 let startButton = document.querySelector('#startButton')
 let tempo = document.querySelector('.time');
 let timerDiv =document.querySelector(".time");
-let scoreHtml = document.querySelector(".score")
-let seconds = 60;
+let scoreHtml = document.querySelector(".score");
+let seconds = 15;
 let score = 0;
 let start = document.querySelector('#startButton')
 let answer = document.querySelector('#user__input')
@@ -18,11 +20,12 @@ function countDown() {
         seconds--;
         tempo.innerHTML = seconds;
         if (seconds === 0) {
+            words.textContent = "Word Here"
             scoreHtml.innerHTML = "0";
             startButton.disabled = false;
             clearInterval(timer);
-            seconds = 60;
-            timerDiv.innerHTML = "60";
+            seconds = 15;
+            timerDiv.innerHTML = "15";
             startButton.disabled = false;	
         }
     }, 1000);
@@ -52,9 +55,9 @@ startButton.addEventListener('click', () => {
 answer.addEventListener('keypress', (e) => {
     if (e.key === 'Enter'){
         if (answer.value == words.textContent){
-            score++
-            scoreHtml.textContent = score
-            answer.value = ''
+            score++;
+            scoreHtml.textContent = score;
+            answer.value = '';
             let word = easyList[Math.floor(Math.random() * easyList.length)]
             console.log(word)
             words.textContent = word;
